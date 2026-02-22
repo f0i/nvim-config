0b018a4392e5fa3d41cbf4c671ccf8f3a73e9ceb
Martin Sigloch <f0i.git@projects.f0i.de>
2026-02-22 11:44:02 +0000

    Update tree-sitter config to new API

--- init.lua
+++ init.lua
@@ -961,43 +961,27 @@ require('lazy').setup({
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
-    main = 'nvim-treesitter.configs', -- Sets main module to use for opts
-    -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
-    opts = {
-      ensure_installed = {
-        'bash',
-        'c',
-        'diff',
-        'html',
-        'lua',
-        'luadoc',
-        'markdown',
-        'markdown_inline',
-        'query',
-        'vim',
-        'vimdoc',
-        'css',
-        'scss',
-        'javascript',
-        'typescript',
-      },
-      -- Autoinstall languages that are not installed
-      auto_install = true,
-      highlight = {
-        enable = true,
-        -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-        --  If you are experiencing weird indenting issues, add the language to
-        --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-        additional_vim_regex_highlighting = { 'ruby' },
-      },
-      indent = { enable = true, disable = { 'ruby' } },
-    },
-    -- There are additional nvim-treesitter modules that you can use to interact
-    -- with nvim-treesitter. You should go explore a few and see what interests you:
-    --
-    --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-    --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-    --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+    config = function()
+      local parsers = require('nvim-treesitter.parsers')
+      parsers.motoko = {
+        install_info = {
+          url = 'https://github.com/christoph-dfinity/tree-sitter-motoko',
+          files = { 'src/parser.c' },
+          branch = 'main',
+        },
+      }
+
+      local filetypes = {
+        'bash', 'c', 'diff', 'html', 'lua', 'luadoc',
+        'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc',
+        'css', 'scss', 'javascript', 'typescript', 'motoko',
+      }
+      require('nvim-treesitter').install(filetypes)
+      vim.api.nvim_create_autocmd('FileType', {
+        pattern = filetypes,
+        callback = function() vim.treesitter.start() end,
+      })
+    end,
   },
 
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
@@ -1049,18 +1033,6 @@ require('lazy').setup({
 })
 
 vim.filetype.add { extension = { mo = 'motoko' } }
-
-local parser_config = require('nvim-treesitter.parsers').get_parser_configs()
-parser_config.motoko = {
-  install_info = {
-    url = 'https://github.com/christoph-dfinity/tree-sitter-motoko', -- local path or git repo
-    files = { 'src/parser.c' }, -- note that some parsers also require src/scanner.c or src/scanner.cc
-    -- optional entries:
-    branch = 'main', -- default branch in case of git repo if different from master
-    generate_requires_npm = false, -- if stand-alone parser without npm dependencies
-    requires_generate_from_grammar = false, -- if folder contains pre-generated src/parser.c
-  },
-}
 
 -- The following will add the additional suffixes, so imports without .mo can be followed with `gf`
 vim.api.nvim_create_autocmd('FileType', {
